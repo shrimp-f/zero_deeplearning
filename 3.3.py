3d13c0f0453eef1840211431e8e3e345d36370e4
shrimp-f <fujitadynamics@gmail.com>
2020-04-28 15:26:26 +0900

    3.3

---
+++ 3.3.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+
+
+def step_function(x):
+    y = x > 0
+    return y.astype(np.int)
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0,x)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = relu(x)
+
+plt.plot(x,y)
+plt.ylim(-0.1, 1.1)
+plt.show()
