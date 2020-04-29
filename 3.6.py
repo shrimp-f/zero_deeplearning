1331487d5ae736ac54651d5decd28378f6723786
shrimp-f <fujitadynamics@gmail.com>
2020-04-29 15:16:09 +0900

    3.6.2

--- 3.6.py
+++ 3.6.py
@@ -6,26 +6,63 @@ from dataset.mnist import load_mnist
 import numpy as np
 import matplotlib.pylab as plt
 from PIL import Image
+import pickle
 
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c) #オーバーフロー対策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+    
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+def get_data():
+    # (訓練画像, 訓練ラベル), (テスト画像, テストラベル)
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    return x_test, t_test
 
-img = x_train[0]
-label = t_train[0]
-print(label)
 
-print(img.shape)
-img = img.reshape(28, 28)
-print(img.shape)
+def init_network():
+    with open("sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
 
-img_show(img)
+    return network
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+if __name__ == "__main__":
+    x, t = get_data()
+    network = init_network()
+
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+        p = np.argmax(y)
+        if p == t[i]:
+            accuracy_cnt += 1
+
+    print("Accuracy:" + str(float(accuracy_cnt)/len(x)))
