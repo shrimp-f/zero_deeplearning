47ed040037397055c167144a33cc8037eb484dea
shrimp-f <fujitadynamics@gmail.com>
2020-05-09 14:10:56 +0900

    4.5.3

--- 4.5.py
+++ 4.5.py
@@ -3,6 +3,7 @@ import numpy as np
 from dataset.mnist import load_mnist
 from common.functions import *
 from common.gradient import numerical_gradient
+from two_layer_net import TwoLayerNet
 
 
 
@@ -53,6 +54,32 @@ class TwoLayerNet:
 
         return grads
 
+    #高速版
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+        
+        batch_num = x.shape[0]
+        
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+        
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+        
+        dz1 = np.dot(dy, W2.T)
+        da1 = sigmoid_grad(a1) * dz1
+        grads['W1'] = np.dot(x.T, da1)
+        grads['b1'] = np.sum(da1, axis=0)
+
+        return grads
+
 
 def main_4_5_1():
     net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
@@ -77,12 +104,15 @@ def main_4_5_1():
 if __name__ == "__main__":
     (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=True)
 
-    train_loss_list = []
-
-    iters_num = 1
+    iters_num = 10000
     train_size = x_train.shape[0]
     batch_size = 100
     learning_rate = 0.1
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    iter_per_epoch = max(train_size / batch_size, 1)
 
     network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -91,7 +121,8 @@ if __name__ == "__main__":
         x_batch = x_train[batch_mask]
         t_batch = t_train[batch_mask]
 
-        grad = network.numerical_gradient(x_batch, t_batch)
+#        grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch)
 
         for key in ('W1', 'b1', 'W2', 'b2'):
             network.params[key] -= learning_rate * grad[key]
@@ -99,4 +130,9 @@ if __name__ == "__main__":
         loss = network.loss(x_batch, t_batch)
         train_loss_list.append(loss)
 
-    print(train_loss_list)
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
