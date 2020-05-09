af838116ed9aa0066e24b54d7545e25ab346c6cc
shrimp-f <fujitadynamics@gmail.com>
2020-05-09 13:54:28 +0900

    4.5.2

--- 4.5.py
+++ 4.5.py
@@ -54,9 +54,7 @@ class TwoLayerNet:
         return grads
 
 
-
-
-if __name__ == "__main__":
+def main_4_5_1():
     net = TwoLayerNet(input_size=784, hidden_size=100, output_size=10)
 
     print(net.params['W1'].shape)
@@ -74,3 +72,31 @@ if __name__ == "__main__":
     print(grads['b1'].shape)
     print(grads['W2'].shape)
     print(grads['b2'].shape)
+
+
+if __name__ == "__main__":
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=True)
+
+    train_loss_list = []
+
+    iters_num = 1
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+    for i in range(iters_num):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+
+        grad = network.numerical_gradient(x_batch, t_batch)
+
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+
+    print(train_loss_list)
