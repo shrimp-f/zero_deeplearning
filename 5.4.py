1ebaf84c1de9ad057b362f16089044d017794a94
shrimp-f <fujitadynamics@gmail.com>
2020-05-11 21:12:57 +0900

    5.4.2

--- 5.4.py
+++ 5.4.py
@@ -23,6 +23,20 @@ class MulLayer:
         return dx, dy
 
 
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
 
 
 if __name__ == "__main__":
