7c10d9b16275a900e280d980065c1f38d32c4753
shrimp-f <fujitadynamics@gmail.com>
2020-05-14 13:22:49 +0900

    5.5.2

--- 5.5.py
+++ 5.5.py
@@ -20,6 +20,17 @@ class Relu:
         return dx
 
 
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
 
 
 x = np.array([[1.0, -0.5], [-2.0, 3.0]])
