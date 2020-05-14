12457b27b53b21836b671265b54b870eb74d7a8f
shrimp-f <fujitadynamics@gmail.com>
2020-05-14 13:58:02 +0900

    5.6.3

--- 5.6.py
+++ 5.6.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from common.functions import softmax, cross_entropy_error
 
 
 class Affine:
@@ -21,4 +21,23 @@ class Affine:
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
 
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
         return dx
+
