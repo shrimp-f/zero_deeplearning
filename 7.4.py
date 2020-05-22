0b460510b85833b27a11a9fa03d786b8ecc6bba1
shrimp-f <fujitadynamics@gmail.com>
2020-05-22 16:27:53 +0900

    7.4.4

--- 7.4.py
+++ 7.4.py
@@ -3,17 +3,6 @@ import numpy as np
 from common.util import im2col
 
 
-if __name__ == "__main__":
-    x1 = np.random.rand(1, 3, 7, 7)
-    col1 = im2col(x1, 5, 5, stride=1, pad=0)
-    print(col1.shape)
-
-    x2 = np.random.rand(10, 3, 7, 7)
-    col2 = im2col(x2, 5, 5, stride=1, pad=0)
-    print(col2.shape)
-
-
-
 
 class Convolution:
     def __init__(self, W, b, stride=1, pad=0):
@@ -35,3 +24,43 @@ class Convolution:
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
 
         return out
+
+
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=1, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / (self.stride))
+        out_w = int(1 + (W - self.pool_w) / (self.stride))
+
+        #(1)展開
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
+        col = col.reshape(-1, self.pool_h*self.pool_w)
+
+        #(2)最大値
+        out = np.max(col, axis=1)
+
+        #(3)整形
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
+
+
+
+
+if __name__ == "__main__":
+    x1 = np.random.rand(1, 3, 7, 7)
+    col1 = im2col(x1, 5, 5, stride=1, pad=0)
+    print(col1.shape)
+
+    x2 = np.random.rand(10, 3, 7, 7)
+    col2 = im2col(x2, 5, 5, stride=1, pad=0)
+    print(col2.shape)
+
