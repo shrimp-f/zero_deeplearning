fe8e9c57180b869f14fd16da9741fa104f37a229
shrimp-f <fujitadynamics@gmail.com>
2020-05-23 23:43:58 +0900

    7.5 1

---
+++ 7.5.py
@@ -0,0 +1,45 @@
+import sys, os
+import numpy as np
+from common.util import im2col
+
+
+
+
+class SimpleConvNet:
+    def __init__(self, input_dim=(1, 28, 28),
+                conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+                hidden_size=100, output_size=10, weight_init_std=0.01):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2*filter_pad) / \
+                            filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
+
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'],
+                                            self.params['b1'],
+                                            conv_param['stride'],
+                                            conv_param['pad'])
+        self.layers['Relu'] = Relu()
+
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine1(self.params['W3'], self.params['b3'])
+
+        self.last_layer = SoftmaxWithLoss()
+
+        
